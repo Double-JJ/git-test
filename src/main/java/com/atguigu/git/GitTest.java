60df52c35926c2822f0454d31af3757e57eeedef
yuesr <yuesr@atguigu.com>
2021-05-18 21:30:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: GitTest
+ * @Author: Double.J
+ * @CreateTime: 2021/5/18 21:25
+ * @Description:
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+}
